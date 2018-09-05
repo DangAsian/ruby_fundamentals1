6e1c1a9ef397d20a13fe924ed8c3e28399991573
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-05 09:31:01 -0400

    new changes to me

--- exercise2.rb
+++ exercise2.rb
@@ -7,4 +7,8 @@ cost_of_dinner = gets.chomp.to_i
 puts "#{cost_of_dinner} $ equates to #{cost_of_dinner * 1.15} dollars"
 
 puts "How was it" + ", was it good?"
-puts "How was it" + 1.to_s
+puts "How was it\s" + 1.to_s
+#this puts a space between both cannitation
+
+puts "I have been living for #{45628*7839} years"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
